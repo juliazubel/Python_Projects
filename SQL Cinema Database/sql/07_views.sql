2f23f8100aa6711b6c3a00588d29a23dbeffd8ad
juliazubel <102976034+juliazubel@users.noreply.github.com>
2026-04-28 21:38:02 +0200

    Refactor views for screenings and available seats
    Updated views to include additional joins and renamed fields for clarity.

--- SQL Cinema Database/sql/07_views.sql
+++ SQL Cinema Database/sql/07_views.sql
@@ -1,13 +1,15 @@
 -- Screenings for a given day
-CREATE OR ALTER VIEW [ScreeningsForAGivenDay] AS
-SELECT IdScreening FROM Screening
-WHERE DateTime = getdate(); SELECT Title,Dimention.ValueDimention, Duration,convert(date,DateTime)
-’Data’ ,convert(time,DateTime)’Godzina’ FROM Screening
+CREATE OR ALTER VIEW [ScreeningsForAGivenDay] 
+AS
+SELECT Title
+		,Dimention.ValueDimention
+		, Duration,convert(date,DateTime) AS 'Date'
+		,convert(time,DateTime) AS 'Hour' 
+FROM Screening
 JOIN Film ON Screening.IdFilm=Film.IdFilm
 JOIN Dimention ON Dimention.IdDimention=Screening.IdDimention
 JOIN Screen ON Screen.IdScreen=Screening.IdScreen
 WHERE DateTime>getdate()-30 AND DateTime<=getdate()+30
-ORDER BY convert(date,DateTime),convert(time,DateTime)
 
 
 -- Movies for kids
@@ -32,10 +34,15 @@ WHERE ProductionYear = 2021;
 -- Available Seats
 CREATE OR ALTER VIEW [AvailableSeats]
 AS
-SELECT Screening.IdScreening, Title, Dimention.ValueDimention, Duration, convert(date,DateTime) ’Data’,
-convert(time,DateTime)’Godzina’, CASE WHEN dbo.fnSeatForScreening(IdScreening)=0 THEN ’Brak miejsc’ ELSE
-’Sprzedaz dostepna’ END
-’Status’,dbo.fnSeatForScreening(IdScreening) ’Kod’ FROM Screening
+SELECT Screening.IdScreening
+	, Title
+	, Dimention.ValueDimention
+	, Duration
+	, convert(date,DateTime) AS 'Date'
+	, convert(time,DateTime) AS 'Hour'
+	, CASE WHEN dbo.fn_AvailableSeats(IdScreening)=0 THEN 'No places' ELSE 'Seats available' END AS 'Available Seats'
+	, dbo.fn_AvailableSeats(IdScreening) AS 'Kod' 
+FROM Screening
 JOIN Film ON Screening.IdFilm=Film.IdFilm
 JOIN Dimention ON Dimention.IdDimention=Screening.IdDimention
 JOIN Screen ON Screen.IdScreen=Screening.IdScreen
